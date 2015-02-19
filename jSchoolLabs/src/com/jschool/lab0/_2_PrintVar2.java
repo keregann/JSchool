6b71493d757ee1941f31cf3dc37acbd301aeabea
keregann <keregann@gmail.com>
2015-02-19 23:53:19 +0200

    Am eliminat variabilele aditionale

--- jSchoolLabs/src/com/jschool/lab0/_2_PrintVar2.java
+++ jSchoolLabs/src/com/jschool/lab0/_2_PrintVar2.java
@@ -7,15 +7,14 @@ public class _2_PrintVar2 {
 	public static void main(String args[]) {
 
 		Scanner print100 = new Scanner(System.in);
-		int i = 1; /*Am declarat mai multe variabile pentru ciclu while, daca ma folosesc de una singura apu ciclu nu lucreaza*/
-		int a = 1;
-		int b = 1;
+		
 		int x;
 
 		System.out.print("Introduceti numar de tip \"int\"  pentru afisare repetata: ");
 		x = print100.nextInt();
-		while (i <= 100) {
-			System.out.print("Valoarea lui x este: " + x + "," + " ");
+		int i = 0;
+		while (i < 100) {
+			System.out.print("Valoarea lui x este: " + x + ", ");
 			i++;
 		}
 
@@ -24,18 +23,20 @@ public class _2_PrintVar2 {
 		float f;
        System.out.println("Introduceti numar de tip \"float\" pentru afisare repetata: ");
         f = print100.nextFloat();
-        while (a <= 100) {
-			System.out.print("Valoarea lui f este: " + f + "," + " ");
-			a++;
+        i = 0;
+        while (i < 100) {
+			System.out.print("Valoarea lui f este: " + f + ", ");
+			i++;
         }
         System.out.print("."+'\n');
         
         String s;
         System.out.println("Introduceti o valoare de tip \"String\" pentru afisare repetata: ");
          s = print100.next();
-         while (b <= 100) {
- 			System.out.print("Valoarea lui s este: " + s + "," + " " );
- 			b++;
+         i = 0;
+         while (i < 100) {
+ 			System.out.print("Valoarea lui s este: " + s + ", ");
+ 			i++;
          }
          System.out.print("."+'\n');
 	}
