74de2ac9ddf10826ffd7fea2f7974d45f702a18d
lexxfirecore <lexxfirecore@gmail.com>
2015-02-20 22:35:28 +0200

    schitza sarcina 4 si 5

---
+++ jSchoolLabs/src/com/jschool/labs/lab0/_4_DatePrint.java
@@ -0,0 +1,18 @@
+package com.jschool.labs.lab0;
+
+/*
+ * De facut un program care printeaza ora curenta la fiecare 5 secunde. 
+ */
+
+public class _4_DatePrint {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		// foloseste un ciclu infinit cu while sau for
+		// citeste data curenta cu new Date() sau new Calendar(), desigur trebuie importate
+		// imprima ora curenta folosind clasa SimpleDateFormat sau functia System.out.format() din exemplul _2_PrintFormat
+		// fa o pauza de 5 sec cu delay ori sleep
+		
+	}
+
+}
---
+++ jSchoolLabs/src/com/jschool/labs/lab0/_5_SumaVector.java
@@ -0,0 +1,24 @@
+package com.jschool.labs.lab0;
+
+/*
+ * De facut un program care calculeaza suma, media aritmetica, radacina patrata din jumatate din media arimetica, numarul minim si maximum 
+ * al unui sir de numere introdus de la tastatura 
+ */
+public class _5_SumaVector {
+
+	public static void main(String[] args) {
+		// se declara un array unidimensional de marime la alegere (n) sau introdus de la tastatura
+		// se citesc elementele de la tastatura si se introduc in vector, ex: _2_CitireVector
+		
+		// se declara variabilele suma, ma, sqma2, max, min
+		// suma se calculeaza adaugind fiecare element al vectorului la variabila suma 
+		// media aritmetica se calculeaza din suma numerelor impartita la numarul de elemente
+		// num max se calculeaza: se considera max orice element, se parcurge matricea si in cazul ca un element este mai mare decit max, 
+		// lui max ii dam valoarea noua gasita, ( pentru min aceiasi logica)
+		
+		// se afiseaza tabloul initial si numarul n		
+		// se afiseaza toate valorile calculate
+
+	}
+
+}
